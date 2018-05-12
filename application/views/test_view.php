70092b46d8e15f18550b30b48235aa79e88c9508
sutriman <sutriman1995@gmail.com>
2018-05-12 09:33:43 +0700

    Initial, I havent copy the worked library form my own project

---
+++ application/views/test_view.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Codeigniter Multilevel Menu - Example Index</title>
+</head>
+<body>
+	<h3>The Ex.</h3>
+	<ul>
+		<li><a href="<?php echo site_url("test/basic") ?>">Basic Example</a></li>
+		<li><a href="<?php echo site_url("test/inject") ?>">Basic Example With inject men item</a></li>
+		<li><a href="<?php echo site_url("test/bootstrap1") ?>">Twitter Bootstrap Example 1</a></li>
+		<li><a href="<?php echo site_url("test/bootstrap2") ?>">Twitter Bootstrap Example 2</a></li>
+	</ul>
+
+	<h3>About</h3>
+	<p>This is just my revision from <a href="https://github.com/edomaru/codeigniter_multilevel_menu">Edomaru's Library</a>, because when I using it, some features not work perfectly. Maybe bcs it's too old and the PHP version was updated.</p>
+
+	<h3>Original Author</h3>
+	<a href="https://github.com/edomaru/">Edomaru</a>
+
+	<h3>Connect with Me</h3>
+	Twit 	: <a href="https://twitter.com/sutriman1995">Sutriman</a>
+	GitHub 	: <a href="https://github.com/sutriman1995/">Sutriman</a>
+	GitLab	: <a href="https://gitlab.com/sutriman">Sutriman</a>
+
+</body>
+</html>
